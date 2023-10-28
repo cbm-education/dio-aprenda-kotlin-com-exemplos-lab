4cc871217b46df6ac14048c86a891e6560c990ea
Cassio Menezes <cassio.menezes@outlook.com.br>
2023-10-28 10:31:05 -0300

    feat: melhorias e testes

--- desafio.kt
+++ desafio.kt
@@ -1,8 +1,8 @@
 enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-data class Usuario(val nome: String)
+data class Usuario(val nome: String, val email: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val nivel: Nivel, val duracao: Int = 50)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
@@ -11,7 +11,7 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     fun matricular(usuario: Usuario) {
         //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
         inscritos.add(usuario)
-        println("Matrícula de ${usuario.nome} realizada com sucesso!")
+        println("Matrícula de ${usuario.nome} no curso $nome realizada com sucesso!")
         println("Conteudos:")
         conteudos.forEach {
             i -> println("- ${i.nome}")
@@ -19,15 +19,19 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     }
 }
 
+fun getFormacaoKotlin(): Formacao {
+    
+    val conteudoKotlin = listOf(ConteudoEducacional("Introdução ao Kotlin", Nivel.BASICO), ConteudoEducacional("Documentacao do Kotlin", Nivel.BASICO), ConteudoEducacional("Orientação a Objeto com Kotlin", Nivel.AVANCADO, 200))
+    
+    return Formacao("Kotlin para Iniciantes", conteudoKotlin)
+}
+
 fun main() {
     //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
-    val conteudoKotlin = mutableListOf<ConteudoEducacional>()
 
-    val aluno1 = Usuario("Joao Maria")
-    val conteudo = ConteudoEducacional("Introdução ao Kotlin")
-    conteudoKotlin.add(conteudo)
+    val aluno1 = Usuario("Joao Maria", "joaomaria@email.eml")
 
-        val formacao1 = Formacao(aluno1.nome, conteudoKotlin)
-        formacao1.matricular(aluno1)
+    val formacaoKotlin = getFormacaoKotlin()
+    formacaoKotlin.matricular(aluno1)
 }
